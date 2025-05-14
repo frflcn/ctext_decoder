ca6a40ba5ca8ba4e4ac2d88c24e10ad86330d653
frflcn <tmstauff@gmail.com>
2025-05-14 17:20:09 -0400

    Bring Charsets to higher-level module

--- src/charsets/mod.rs
+++ src/charsets/mod.rs
@@ -1,7 +1,24 @@
-pub mod gb_2312;
-pub mod jis_x0208;
-pub mod ks_c5601;
-pub mod iso_8859;
-pub mod jis_x0201;
+mod gb_2312;
+mod jis_x0208;
+mod ks_c5601;
+mod iso_8859;
+mod jis_x0201;
 #[cfg(test)]
 pub mod tests;
+
+
+pub use iso_8859::ISO_8859_1;
+pub use iso_8859::ISO_8859_2;
+pub use iso_8859::ISO_8859_3;
+pub use iso_8859::ISO_8859_4;
+pub use iso_8859::ISO_8859_5;
+pub use iso_8859::ISO_8859_6;
+pub use iso_8859::ISO_8859_7;
+pub use iso_8859::ISO_8859_8;
+pub use iso_8859::ISO_8859_9;
+
+pub use jis_x0201::JIS_X0201;
+
+pub use jis_x0208::JIS_X0208;
+pub use gb_2312::GB_2312;
+pub use ks_c5601::KS_C5601;
